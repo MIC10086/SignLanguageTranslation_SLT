7bf4943a7b63608050377b3c3ce4b0d691076d91
Edgar RP <jefelitman@gmail.com>
2020-04-12 14:04:15 +0000

    Build_Datasets v1.1

--- utils/build_tf_data.py
+++ utils/build_tf_data.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 from .DatasetsLoaderUtils import flow_from_tablePaths
 from .data_augmentation import frame_sampling
 
@@ -8,18 +9,20 @@ def build_datasets(table_paths, args):
     train_gen = raw_data.data_generator(1, args.inputShape[-1])
     def train_gen_sampling():
         for v, l in train_gen:
+            s = np.r_[[int(j) for j in (raw_data.to_class[l]).split(", ")]]
             for new_v in frame_sampling(v, args.inputShape[0]):
-                yield new_v, l
-    train_data = tf.data.Dataset.from_generator(train_gen_sampling, (tf.float32, tf.int64),
+                yield [new_v, s], s
+    train_data = tf.data.Dataset.from_generator(train_gen_sampling, ([tf.float32, tf.int64], tf.int64),
         (args.inputShape, [])
         )
 
     test_gen = raw_data.data_generator(2, args.inputShape[-1])
     def test_gen_sampling():
         for v, l in test_gen:
+            s = np.r_[[int(j) for j in (raw_data.to_class[l]).split(", ")]]
             for new_v in frame_sampling(v, args.inputShape[0]):
-                yield new_v, l
-    test_data = tf.data.Dataset.from_generator(test_gen_sampling, (tf.float32, tf.int64),
+                yield [new_v, s], s
+    test_data = tf.data.Dataset.from_generator(test_gen_sampling, ([tf.float32, tf.int64], tf.int64),
         (args.inputShape, [])
         )
 
@@ -27,9 +30,10 @@ def build_datasets(table_paths, args):
         dev_gen = raw_data.data_generator(3, args.inputShape[-1])
         def dev_gen_sampling():
             for v, l in dev_gen:
+                s = np.r_[[int(j) for j in (raw_data.to_class[l]).split(", ")]]
                 for new_v in frame_sampling(v, args.inputShape[0]):
-                    yield new_v, l
-        dev_data = tf.data.Dataset.from_generator(dev_gen_sampling, (tf.float32, tf.int64),
+                    yield [new_v, s], s
+        dev_data = tf.data.Dataset.from_generator(dev_gen_sampling, ([tf.float32, tf.int64], tf.int64),
             (args.inputShape, [])
             )
     except:
