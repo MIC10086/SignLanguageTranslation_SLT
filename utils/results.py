60058e252a59048ac422d0bfad7bfeb4ba6a3ed2
Edgar RP <jefelitman@gmail.com>
2020-04-13 22:35:48 -0500

    Results v1.5

--- utils/results.py
+++ utils/results.py
@@ -21,6 +21,10 @@ def __load_frame__(frame_path, size = None, channels = 3):
             return img
 
 def save_predictions(model, path_to_save, vocab, args, train_data, test_data, dev_data = None):
+    """train_data, test_data and dev_data must be a tensorflow Dataset returning (video, 
+        sentence, path_video) respectively. The tensorflow dataset must be batched with 
+        and only with one example.
+    """
 
     results = {}
 
@@ -33,20 +37,23 @@ def save_predictions(model, path_to_save, vocab, args, train_data, test_data, de
     for type_data, gen in types_data:
         results[type_data] = {}
         for video, sentence, path in gen:
+            # Check for batch size to be 1
+            if video.shape[0] != 1 or sentence.shape[0] != 1 or path.shape[0] != 1:
+                raise AssertionError('The data in the generator is bad batched...')
+
             # Convert the array to word
-            target_sentence = [index_word[i] for i in sentence][1:]
-            print("Video: " + path)
+            target_sentence = [index_word[i] for i in sentence[0].numpy()][1:]
+            print("Video: " + path[0].numpy().decode('utf-8'))
             print("Reference: " + " ".join(target_sentence))
 
             # Predict in the model
-            prediction_indexes = model.predict((np.r_[[video]], 
-                np.r_[[sentence[:-1]]]))
+            prediction_indexes = model.predict((video, sentence[:,:-1]))
             prediction_indexes = np.argmax(prediction_indexes[0], axis=1)
             prediction_sentence = [index_word[i] for i in prediction_indexes]
             
             print("Translation: "+" ".join(prediction_sentence)+"\n")
 
-            results[type_data][path] = {"prediction_sentence" : prediction_sentence,
+            results[type_data][path[0].numpy().decode('utf-8')] = {"prediction_sentence" : prediction_sentence,
                 "target_sentence" : target_sentence
                 }
     
